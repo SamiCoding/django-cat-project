c07dc02b0aa6655494138465fa17481cd95eb1f1
SamiCoding <samicoding@gmail.com>
2023-10-31 16:23:52 +0900

    Add model to admin

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event, DefaultLuckyItem, LuckyItem, DefaultWeapon, Weapon, Character, Enemy
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(DefaultLuckyItem)
+admin.site.register(LuckyItem)
+admin.site.register(DefaultWeapon)
+admin.site.register(Weapon)
+admin.site.register(Character)
+admin.site.register(Enemy)
--- postings/admin.py
+++ postings/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Posting, Comment
 
-# Register your models here.
+admin.site.register(Posting)
+admin.site.register(Comment)
